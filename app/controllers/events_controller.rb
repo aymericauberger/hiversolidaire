e68035aa73518b1597624afe890f763f5634e15b
Aymeric Auberger <aymeric.auberger@gmail.com>
2017-12-02 22:32:09 +0100

    Update sjdhp start

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 # PUBLIC
 class EventsController < ApplicationController
   def index
-    @start_date = ENV['APP_NAME'] == 'Saint-Jacques du Haut-Pas' ? Date.new(2018, 1, 1) : Date.new(2017, 12, 17)
+    @start_date = ENV['APP_NAME'] == 'Saint-Jacques du Haut-Pas' ? Date.new(2018, 1, 5) : Date.new(2017, 12, 17)
     date = [@start_date, Date.today].max
     month = params[:month].presence ||
             l(date, format: '%B %Y').humanize
