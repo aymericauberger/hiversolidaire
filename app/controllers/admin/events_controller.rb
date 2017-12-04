86452e51059c649caafe48e050642d1d7dfa7b61
Aymeric Auberger <aymeric.auberger@gmail.com>
2017-12-04 18:03:22 +0100

    Fix admin

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -2,12 +2,13 @@ module Admin
   # ADMIN
   class EventsController < Admin::BaseController
     def index
-      date = [Date.new(2017, 12, 1), Date.today].max
+      @start_date = ENV['APP_NAME'] == 'Saint-Jacques du Haut-Pas' ? Date.new(2018, 1, 5) : Date.new(2017, 12, 17)
+      date = [@start_date, Date.today].max
       month = params[:month].presence ||
               l(date, format: '%B %Y').humanize
       month = month.sub('Décembre', 'Dec').sub('Février', 'Feb')
 
-      @from_date = month.to_date.beginning_of_month
+      @from_date = [@start_date, month.to_date.beginning_of_month].max
       @to_date = month.to_date.end_of_month
     end
 
